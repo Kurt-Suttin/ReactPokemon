3ba4f4d517fd69eee4dd5cf9e1e76081d9b4877c
Kurt Suttin <Kurtlesuttin@gmail.com>
2024-05-15 19:06:10 -0500

    improved  the buttons and current page number

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -122,15 +122,16 @@ const PokemonList = () => {
                         </div>
                     )}
                     <div className="pagination">
-                        <p className={"page-number"}>{currentPage}</p>
+                        <p className={"page-number"}>{currentPage - 1}</p>
                         <div className={"page-buttons"}>
-                            <button onClick={prevPageTen} disabled={currentPage === 1}>Previous 10</button>
+                            <button onClick={prevPageTen} disabled={currentPage <= 10}>Previous 10</button>
                             <button onClick={prevPage} disabled={currentPage === 1}>Previous</button>
                             <button onClick={firstPage} disabled={currentPage === 1}>1st Page</button>
                             <button onClick={nextPage} disabled={currentPage === totalPages}>Next</button>
                             <button onClick={nextPageTen} disabled={currentPage === totalPages}>Next 10</button>
                         </div>
                     </div>
+
                 </div>
             </div>
             {selectedPokemon && (
